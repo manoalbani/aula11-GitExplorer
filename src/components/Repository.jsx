f58c80acbaa2ba8e0a017a5f4eac15d6555444e4
André Manoel Albani Cezar <andre.11952@sesc-sc.com.br>
2023-05-26 19:24:10 -0300

    Rotas

---
+++ src/components/Repository.jsx
@@ -0,0 +1,6 @@
+function Repository(){
+    return (
+        <h1>repo</h1>
+    );
+};
+export default Repository;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
+import Repository from './components/Repository.jsx'
 import './index.css'
+import { BrowserRouter ,Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+         
+          <Routes>
+         <Route path="/" element={<App/>} />
+         <Route path="/repo" element={<Repository/>} />
+         </Routes>
+       
+       </BrowserRouter>
+
   </React.StrictMode>,
 )
